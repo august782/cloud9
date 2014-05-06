1c726ff052395b3f661eb55aa8c01363f6f8963c
awshi2 <awshi2@awshi2-VirtualBox.(none)>
2014-05-05 19:44:32 -0500

    Modified build to use clang, which is more of what cloud9 does

--- cs526/g33/build.sh
+++ cs526/g33/build.sh
@@ -1,6 +1,6 @@
 # Run this in the directory it's in
 
 rm -f *.bc
-for f in `ls *.c`; do llvm-gcc -I ../../include -g -c -emit-llvm $f -o ${f/.c/.bc} ; done
+for f in `ls *.c`; do clang -I ../../include -g -c -emit-llvm $f -o ${f/.c/.bc} ; done
 llvm-link -o Final.bc *.bc
-klee --libc=uclibc --posix-runtime Final.bc
+#klee --libc=uclibc --posix-runtime Final.bc
